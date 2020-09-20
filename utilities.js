35d7a6e7a11ee20d2c359b6b5dc0943b35535695
gloomykevin <1293535916@qq.com>
2020-09-20 21:37:48 +0800

    complete template

--- utilities.js
+++ utilities.js
@@ -1,20 +1,79 @@
 (function () {
+    // 从将 _ 设置为一个"空对象", 的思路转变到 _ 可以是一个"函数"
+    var root = (typeof self == 'object' && self.self == self && self) ||
+        (typeof global == 'object' && global.global == global && global) ||
+        this || {};
 
-    // 函数初始化设置
-    var root = _;
+    // 为array-demo添加代码
+    var ArrayProto = Array.prototype;
+    var push = ArrayProto.push;
 
-    // 函数的所有方法添加在proxyObj上
-    var _ = {};
+    var _ = function (obj) {
+        if (obj instanceof _) return obj;
+        if (!(this instanceof _)) return new _(obj);
+        this._wrapped = obj;
+    };
 
-    // 再将proxyObj挂载到全局
-    // root 的值, 客户端为 `window`, 服务端(node) 中为 `exports`
+    if (typeof exports != 'undefined' && !exports.nodeType) {
+        if (typeof module != 'undefined' && !module.nodeType && module.exports) {
+            exports = module.exports = _;
+        }
+        exports._ = _;
+    } else {
+        root._ = _;
+    }
+
+    _.VERSION = '0.1';
+
+    var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
+
+    var isArrayLike = function (collection) {
+        var length = collection.length;
+        return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
+    };
+
+    _.each = function (obj, callback) {
+        var length, i = 0;
+
+        if (isArrayLike(obj)) {
+            length = obj.length;
+            for (; i < length; i++) {
+                if (callback.call(obj[i], obj[i], i) === false) {
+                    break;
+                }
+            }
+        } else {
+            for (i in obj) {
+                if (callback.call(obj[i], obj[i], i) === false) {
+                    break;
+                }
+            }
+        }
+
+        return obj;
+    }
+
+        .isFunction = function (obj) {
+            return typeof obj == 'function' || false;
+        };
+
+    _.functions = function (obj) {
+        var names = [];
+        for (var key in obj) {
+            if (_.isFunction(obj[key])) names.push(key);
+        }
+        return names.sort();
+    };
+
+    var _ = function (obj) {
+        if (!(this instanceof _)) return new _(obj);
+        this._wrapped = obj;
+    }
+
     root._ = _;
-    // 所以为了node的兼容性,放弃下面这种容易想到的方法
-    // window.proxyObj = proxyObj;
 
-    // 添加自己的方法
-    _.reverse = function (string) {
-        return string.split('').reverse().join('');
+    _.log = function () {
+        console.log(1)
     }
 
     // 先获取到_上的方法
@@ -25,6 +84,11 @@
         }
         return names.sort();
     };
+
+    // 自定义方法写在_.mixin之前
+    _.reverse = function (string) {
+        return string.split('').reverse().join('');
+    }
 
     // 将_上的方法复制到_.prototype
     _mixin = function (obj) {
@@ -45,9 +109,7 @@
         })
         return _;
     };
+
     _mixin(_);
 
 })()
-
-let result = proxyObj.reverse('testFunc');
-console.log(result);
