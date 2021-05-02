64db81826297842e3e4b1bcfb9823ba5f8f478e1
gloomykevin <1293535916@qq.com>
2021-05-02 23:50:56 +0800

    init 深浅拷贝

---
+++ 深浅拷贝/数组的深浅拷贝.js
@@ -0,0 +1,13 @@
+// 浅拷贝
+// 1. concat
+
+// 2. slice
+
+// 深拷贝
+// 1. stringify & parse
+// 问题：
+// 1. 如果数组元素含 undefined/symbol/function，则会被忽略
+// 2. 不能解决循环引用的对象
+// 3. 不能处理正则
+// 4. 不能处理 new Date()
+
